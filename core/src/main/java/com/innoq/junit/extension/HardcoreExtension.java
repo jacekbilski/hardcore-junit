22f79ca1d6c6c136e13561dd7f310c6f19e00e15
Jacek Bilski <jacek.bilski@innoq.com>
2022-07-01 17:23:32 +0200

    First simple extension and a bit of Gradle project structure.

---
+++ core/src/main/java/com/innoq/junit/extension/HardcoreExtension.java
@@ -0,0 +1,16 @@
+package com.innoq.junit.extension;
+
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
+
+public class HardcoreExtension implements BeforeAllCallback {
+
+	private static final Logger logger = LoggerFactory.getLogger(HardcoreExtension.class);
+
+	@Override
+	public void beforeAll(ExtensionContext context) throws Exception {
+		logger.info(() -> "Hello World!");
+	}
+}
