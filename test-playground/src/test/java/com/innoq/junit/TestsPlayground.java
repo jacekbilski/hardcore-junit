5e8b59f61938a819f7eb51bae9800d6c2f431ee7
Jacek Bilski <jacek.bilski@innoq.com>
2022-07-01 18:53:56 +0200

    Trying to use ServiceLoader to register the extension.

--- test-playground/src/test/java/com/innoq/junit/TestsPlayground.java
+++ test-playground/src/test/java/com/innoq/junit/TestsPlayground.java
@@ -1,11 +1,8 @@
 package com.innoq.junit;
 
-import com.innoq.junit.extension.HardcoreExtension;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 
-@ExtendWith(HardcoreExtension.class)
 public class TestsPlayground {
 
 	@Test
